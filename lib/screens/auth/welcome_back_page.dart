cc779d5a6f44397af3ceea8ae1b622ba2b289600
Abdullah Sharef <38993855+Abdullah104@users.noreply.github.com>
2024-04-26 22:53:00 +0300

    .

---
+++ lib/screens/auth/welcome_back_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class WelcomeBackPage extends StatefulWidget {
+  const WelcomeBackPage({super.key});
+
+  @override
+  State<WelcomeBackPage> createState() => _WelcomeBackPageState();
+}
+
+class _WelcomeBackPageState extends State<WelcomeBackPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/screens/splash_page.dart
+++ lib/screens/splash_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import '../app_properties.dart';
+import 'auth/welcome_back_page.dart';
+
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
@@ -7,9 +10,83 @@ class SplashScreen extends StatefulWidget {
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  late Animation<double> opacity;
+  late AnimationController controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller = AnimationController(
+      duration: const Duration(milliseconds: 2500),
+      vsync: this,
+    );
+
+    opacity = Tween<double>(begin: 1.0, end: 0.0).animate(controller)
+      ..addListener(() {
+        setState(() {});
+      });
+    controller.forward().then((_) {
+      navigationPage();
+    });
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  void navigationPage() {
+    Navigator.of(context).pushReplacement(
+      MaterialPageRoute(builder: (_) => const WelcomeBackPage()),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    print('dsafijpasfdpifojasdpjidsapsadfij');
+    return Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('assets/background.jpg'),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Container(
+        decoration: const BoxDecoration(color: transparentYellow),
+        child: SafeArea(
+          child: Scaffold(
+            body: Column(
+              children: <Widget>[
+                Expanded(
+                  child: Opacity(
+                    opacity: opacity.value,
+                    child: Image.asset('assets/logo.png'),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: RichText(
+                    text: const TextSpan(
+                      style: TextStyle(color: Colors.black),
+                      children: [
+                        TextSpan(text: 'Powered by '),
+                        TextSpan(
+                          text: 'int2.io',
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        )
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
