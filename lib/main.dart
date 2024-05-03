5208c1d48ef436264ef501002bdcc9a729404358
Abdullah Sharef <38993855+Abdullah104@users.noreply.github.com>
2024-05-03 16:14:41 +0300

    App initialization.

---
+++ lib/main.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+import 'shope.dart';
+
+void main() => runApp(const Shope());
---
+++ lib/shope.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Shope extends StatelessWidget {
+  const Shope({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
