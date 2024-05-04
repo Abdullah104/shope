cfd269ff98fffbc1ba45cb48974c07072608963d
Abdullah Sharef <38993855+Abdullah104@users.noreply.github.com>
2024-05-04 08:33:05 +0300

    Shope.dart

--- lib/shope.dart
+++ lib/shope.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
 
+import 'screens/splash_screen.dart';
+
 class Shope extends StatelessWidget {
   const Shope({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      title: 'Shope',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: Brightness.light,
+        canvasColor: Colors.transparent,
+        primarySwatch: Colors.blue,
+        fontFamily: 'Montserrat',
+      ),
+      home: const SplashScreen(),
+    );
   }
 }
